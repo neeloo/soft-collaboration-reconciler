bf2df851ee85c1b68732663c5a7d31770a24caaa
neeloo patel <nilusingh945164@gmai.com>
2026-08-25 11:16:39 +0530

    Initial commit

---
+++ src/main/java/com/Neeloo/soft_collaboration_reconciler/SoftCollaborationReconcilerApplication.java
@@ -0,0 +1,16 @@
+package com.Neeloo.soft_collaboration_reconciler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+@EnableScheduling
+@SpringBootApplication
+public class SoftCollaborationReconcilerApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(SoftCollaborationReconcilerApplication.class, args);
+        System.out.println("Hello");
+	}
+
+}
---
+++ src/test/java/com/Neeloo/soft_collaboration_reconciler/SoftCollaborationReconcilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Neeloo.soft_collaboration_reconciler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftCollaborationReconcilerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
